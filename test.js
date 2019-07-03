fd121ff3f8352f9f0b1169168d6b83b0a5c2ce48
Nicolas CUADROS <nicolas.cuadros@etu.univ-nantes.fr>
2019-07-03 11:42:07 +0200

    Changing query.

--- test.js
+++ test.js
@@ -8,26 +8,27 @@ let start = async function () {
 	 * With an amount of stars superior to 0
 	 * With a step of 1 day between each sub request of repository
 	 */
-	const chainName = "testChain"
+	const chainName = "AkkaProjects"
+	const collectionName = "queryChain"
 			queryChain(chainName, {
-				keywords: "akka",
+				keywords: "",
 				language: "scala",
-				begin: new Date("2019-06-01"),
-				end: new Date("2019-06-02"),
+				begin: new Date("2019-01-01"),
+				end: new Date("2019-07-01"),
 				stars: ">0",
 				step: 1
 				})
 				.checkFile({
-						fileName: "",
-						fileExtension: "",
+					fileName:"build",
+					fileExtension:"sbt"
+				},"sbt")
+				.checkFile({
 						keywords: "akka-actor"
-				}, "actor", false) //Check if the repository contain a build.sbt file
+				}, "actor") //Check if the repository contain a build.sbt file
 								// and if it contain the akka-actor keyword
 				.checkFile({
-					fileName: "",
-					fileExtension: "",
 					keywords: "akka-test"
-				}, "test",true)
+				}, "test")
 				//.checkProperty(r=>true)//r.properties.actor.valid && r.properties.test.valid)
 				.clone("./results")	//Clone the repository
 									//Add the fullPath property to the repository
@@ -43,7 +44,7 @@ let start = async function () {
 						var url = "mongodb://localhost:27017/";
 						MongoClient.connect(url,async  function (err, db) {
 							if (err) throw err;
-							var dbo = db.db("queryChain");
+							var dbo = db.db(queryChain);
 							try{
 								await dbo.dropCollection(chainName)
 							}
