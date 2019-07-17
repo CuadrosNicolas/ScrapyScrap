bcf2e6d1fb870c299083b804e0bc2bcf2ae78d3e
Nicolas CUADROS <nicolas.cuadros@etu.univ-nantes.fr>
2019-07-17 13:21:49 +0200

    Testing search for flaky fixing.

--- Parse_testSpawn.js
+++ Parse_testSpawn.js
@@ -3,8 +3,8 @@ const fs = require("fs")
 
 let temp = JSON.parse(fs.readFileSync("./results_AkkaProjects_post.json"))
 let tab = Object.keys(temp).map(key=>{
-	return temp[key].properties.commit.files.map(c=>`${c.commit.author.name.replace(/\n/g,"")}>>>${c.html_url}\n`).join("")
-}).join("");
+	return key+" "+temp[key].properties.fullPath+" "+temp[key].name
+}).join("\n");
 fs.writeFileSync("./list.csv",tab)
 
 
